3b3f64d277d874fdff4991db43b512c06fbe6542
upwan <Upwan.Gupta@cognizant.com>
2021-07-13 11:33:19 +0100

    Removed initial version

--- src/main/java/com/java/example/transfers/infrastructure/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.java.example.transfers.infrastructure;
-
-import com.java.example.transfers.domain.Transaction;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends CrudRepository<Transaction,String> {
-}
